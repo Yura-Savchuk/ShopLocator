e268b2e75e468d17fff3bf8e23f57a5f49b17aa4
Yura Savchuk <yura.savchuk22@gmail.com>
2017-01-30 00:00:26 +0200

    Prerare librarries and clases for testing.

---
+++ app/src/test/java/com/example/shoplocator/buissines/createAndEditShop/CreateAndEditShopInteractorTest.java
@@ -0,0 +1,8 @@
+package com.example.shoplocator.buissines.createAndEditShop;
+
+/**
+ * Created by {@author yura.savchuk22@gmail.com} on 29.01.17.
+ */
+
+public class CreateAndEditShopInteractorTest {
+}
---
+++ app/src/test/java/com/example/shoplocator/buissines/shopDetail/ShopDetailInteractorTest.java
@@ -0,0 +1,8 @@
+package com.example.shoplocator.buissines.shopDetail;
+
+/**
+ * Created by {@author yura.savchuk22@gmail.com} on 29.01.17.
+ */
+
+public class ShopDetailInteractorTest {
+}
